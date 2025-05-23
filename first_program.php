9f7bf83ddfd78e3a785a4686816429a90fb0143c
dawidplatek <dawidplatek.eu.pl@gmail.com>
2025-05-23 19:38:01 +0200

    first commit

---
+++ first_program.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    echo("Hello World 1");
+    // echo("Hello World 2"); // komentarz jednoliniowy
+    echo("Hello World 3");
+    /*echo("Hello World 4");
+    echo("Hello World 5");*/ // komentarz wieloliniowy
+    echo("Hello World 6");
+?>
+
+</body>
+</html>
