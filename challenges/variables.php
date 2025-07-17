4fa8b7c9676ee18c22e4fcebe95964f8ad5e2396
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-07-17 15:58:35 +0200

    True ID - task

---
+++ challenges/variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$titleEMail = "Witaj w kursie PHP!";
+$textMail = "Cieszymy się, że dołączyłeś do naszego kursu PHP. W tym kursie nauczysz się podstaw programowania w tym języku, a także zaawansowanych technik, które pozwolą Ci tworzyć dynamiczne strony internetowe i aplikacje.";
+$emailRecipient = "adam@text.pl";
+$emailSender = "jas@text.pl";
+$emailDate = "2023-10-01";
+
+?>
+
+</body>
+</html>
