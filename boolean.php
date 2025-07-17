e1697c0a4cd408d645ea47552fe74d24f523d209
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-07-17 16:44:06 +0200

    Logical values - boolean

---
+++ boolean.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$emailSent = FALSE; // zmienna logiczna, która określa, czy e-mail został wysłany
+
+$flagResult = 10 > 23; // FALSE
+$result = 5 > 0; // TRUE
+
+echo($result . "<br>"); // TRUE na 1
+echo(FALSE);// FALSE na "nic"
+var_dump($flagResult); // wyświetla typ i wartość zmiennej, w tym przypadku bool(false)
+
+?>
+
+</body>
+</html>
